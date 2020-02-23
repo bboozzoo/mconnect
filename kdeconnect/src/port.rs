b2744dfd033475371a94ce1f7120e462d76ea591
Maciek Borzecki <maciek.borzecki@gmail.com>
2020-02-23 22:35:13 +0100

    kdeconnect: export packet and port
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

--- kdeconnect/src/port.rs
+++ kdeconnect/src/port.rs
@@ -19,9 +19,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-pub mod packet;
-pub mod port;
 
-pub fn bang() {
-    println!("bang!");
-}
+/// Default discovery port
+pub const DISCOVERY : u16 = 1716;
