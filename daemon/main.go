c0c8b08f8139fdfd15c1f7ba8a2d01a323893620
Maciek Borzecki <maciek.borzecki@gmail.com>
2022-01-06 17:06:54 +0100

    daemon: maybe daemon loop
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

---
+++ daemon/main.go
@@ -0,0 +1,43 @@
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+package daemon
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bboozzoo/mconnect/logger"
+)
+
+type Daemon struct{}
+
+func New() *Daemon {
+	return &Daemon{}
+}
+
+func (d *Daemon) Run(ctx context.Context) {
+	log := logger.FromContext(ctx)
+
+	sigChan := make(chan os.Signal)
+
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	run := true
+	for run {
+		select {
+		case sig := <-sigChan:
+			log.Infof("exiting on %v signal", sig)
+			run = false
+		}
+	}
+}
